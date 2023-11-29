f577322587150c907778c1faeb28764080541f92
cpl121 <cpldev121@gmail.com>
2023-11-29 19:53:44 +0100

    feat: init project

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			animation: {
+				'spin-slow': 'spin 3s linear infinite',
+			  }
+		},
+	},
+	plugins: [],
+}
